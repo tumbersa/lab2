ac7749b016f20e2c1e058387044c1be4967c775a
tumbersa <gleb.kapustin.95@gmail.com>
2023-10-28 10:29:46 +0300

    прошлый пуш был инициализирующим(

--- src/main/java/org/example/ShuntingYard.java
+++ src/main/java/org/example/ShuntingYard.java
@@ -1,2 +1,43 @@
-package org.example;public class ShuntingYard {
+package org.example;
+
+//Арифметика
+//Шаг 1
+import java.util.*;
+public class ShuntingYard {
+    private static final String OPERATORS = "*/+-";
+    private static final Map<Character, Integer> PRECEDENCE = Map.of(
+            '*', 2,
+            '/', 2,
+            '+', 1,
+            '-', 1
+    );
+
+    public static String infixToPostfix(String infix) {
+        StringBuilder output = new StringBuilder();
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char token : infix.toCharArray()) {
+            if (Character.isDigit(token)) {
+                output.append(token);
+            } else if (OPERATORS.contains(String.valueOf(token))) {
+                while (!stack.isEmpty() && stack.peek() != '(' &&
+                        PRECEDENCE.get(token) <= PRECEDENCE.get(stack.peek())) {
+                    output.append(' ').append(stack.pop());
+                }
+                output.append(' ');
+                stack.push(token);
+            } else if (token == '(') {
+                stack.push(token);
+            } else if (token == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    output.append(' ').append(stack.pop());
+                }
+                stack.pop();
+            }
+        }
+        while (!stack.isEmpty()) {
+            output.append(' ').append(stack.pop());
+        }
+        return output.toString();
+    }
 }
+
